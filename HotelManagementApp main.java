6e548fbb269173a6153ea0788651c5a386529ae7
AshfakUddin07 <150894506+AshfakUddin07@users.noreply.github.com>
2023-11-16 04:58:11 +0600

    HotelManagementApp main.java

--- HotelManagementApp main.java
+++ HotelManagementApp main.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
 package javafxapplication5;
+
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.application.Application;
 import javafx.geometry.Pos;
